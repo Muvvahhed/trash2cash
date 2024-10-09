07af01d66c1507f4f4f1e8cd785e1fa7dc65be0f
Muwaheed <muwaheedmustapha@gmail.com>
2024-10-09 07:42:28 -0700

    changes

--- app/(dashboard)/marketplace/page.tsx
+++ app/(dashboard)/marketplace/page.tsx
@@ -1,34 +1,11 @@
 import Marketplace from '@/components/MarketPlace'
 import { prisma } from '@/utils/db'
-import { Trash, User } from '@prisma/client'
-import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import React from 'react'
 
-// Use getServerSideProps with typed context
-export const getServerSideProps: GetServerSideProps = async (
-	context: GetServerSidePropsContext
-) => {
-	// Set Cache-Control headers to prevent caching
-	context.res.setHeader(
-		'Cache-Control',
-		'no-store, no-cache, must-revalidate, proxy-revalidate'
-	)
-	context.res.setHeader('Pragma', 'no-cache')
-	context.res.setHeader('Expires', '0')
+export const dynamic = 'force-dynamic'
 
-	// Fetch the trash entries
+async function MartketPlacePage() {
 	const trashEntries = await prisma.trash.findMany({ include: { owner: true } })
-
-	return {
-		props: { trashEntries },
-	}
-}
-
-function MartketPlacePage({
-	trashEntries,
-}: {
-	trashEntries: ({ owner: User } & Trash)[]
-}) {
 	return <Marketplace trashEntries={trashEntries} />
 }
 
